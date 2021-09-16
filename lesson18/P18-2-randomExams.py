952d29e95d52578cd3d7c81dedf4521360b06475
chilly <2675730332@qq.com>
2021-09-16 17:10:11 +0800

    I love the course Financial Modeling and Quantitative Investment ~

--- lesson18/P18-2-randomExams.py
+++ lesson18/P18-2-randomExams.py
@@ -4,18 +4,18 @@ import random
 sourceDoc = docx.Document("题库.docx")
 firstList = []
 secondList = []
-for i in range(0,12):
+for i in range(0, 12):
     para = sourceDoc.paragraphs[i].text
     firstList.append(para)
-for i in range(14,22):
+for i in range(14, 22):
     para = sourceDoc.paragraphs[i].text
     secondList.append(para)
 
-for i in range(0,72):
+for i in range(0, 72):
     doc = docx.Document("试卷模版.docx")
     testQuestionList1 = random.sample(firstList, 6)
     testQuestionList2 = random.sample(secondList, 4)
-    testQuestionList = testQuestionList1 +testQuestionList2
+    testQuestionList = testQuestionList1 + testQuestionList2
     for idx, question in enumerate(testQuestionList):
         doc.paragraphs[idx].add_run(question)
     doc.save(f"examination/试卷_{i+1}.docx")
